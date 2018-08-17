b48375e428993466ef92235cf4db4fb7783d152f
Gleb Ignatiev <glebignatieff@gmail.com>
2018-08-17 23:40:01 +0300

    Minor changes added to main.cpp
    - Text messages on actions
    - "Insert" stub
    - Usage message fixed

--- src/main.cpp
+++ src/main.cpp
@@ -12,42 +12,49 @@ int main(int argc, char** argv)
 
     auto action = parser.action();
 
-    if (action == Parser::Action::HELP)
+    // TODO: handle errors
+
+    if (action == Parser::Action::Help)
     {
+        // TODO
         cout << "Help message is here" << endl;
     }
-    else if (action == Parser::Action::ARCHIVE)
+    else if (action == Parser::Action::Archive)
     {
+        cout << "Archiving data..." << endl;
         auto inputPath = parser.getInputPath();
         auto outputPath = parser.getOutputPath();
         Archiver archiver;
         archiver.archive(inputPath, outputPath);
     }
-    else if (action == Parser::Action::EXTRACT)
+    else if (action == Parser::Action::Extract)
     {
+        cout << "Extracting data..." << endl;
         auto inputPath = parser.getInputPath();
         auto outputPath = parser.getOutputPath();
         Archiver archiver;
         archiver.extract(inputPath, outputPath);
     }
-    else if (action == Parser::Action::LIST)
+    else if (action == Parser::Action::List)
     {
         auto inputPath = parser.getInputPath();
         Archiver archiver;
         archiver.list(inputPath);
     }
-    else if (action == Parser::Action::TOO_FEW_ARGS)
+    else if (action == Parser::Action::Insert)
+    {
+        // TODO
+    }
+    else if (action == Parser::Action::TooFewArgs)
     {
         cout << "Error: Too few arguments!" << endl
-            << "Usage: yat -axl input_path [-o output_path]" << endl
+            << "Usage: yat -axli (input_path | archive) [-o (output_path | input_path)]" << endl
             << "To get help use: yat -h" << endl;
     }
-    else if (action == Parser::Action::ERROR)
+    else if (action == Parser::Action::Error)
     {
         cout << "Error: unknown!" << endl << "Please, refer to help: yat -h";
     }
-
-    system("pause");
 
     return 0;
 }
